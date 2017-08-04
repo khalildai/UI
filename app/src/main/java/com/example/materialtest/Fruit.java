7348c300ae064a1c3b8099e6917f14cd771a822a
khalildai <414204170@qq.com>
2017-08-04 16:34:51 +0800

    UI

---
+++ app/src/main/java/com/example/materialtest/Fruit.java
@@ -0,0 +1,26 @@
+package com.example.materialtest;
+
+/**
+ * Created by DAI on 2017-08-04.
+ */
+
+public class Fruit {
+
+    private String name;
+
+    private int imgageId;
+
+    public Fruit(String name, int imgageId) {
+        this.name = name;
+        this.imgageId = imgageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImgageId() {
+        return imgageId;
+    }
+
+}
